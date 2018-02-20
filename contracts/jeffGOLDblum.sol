f7d598c3a478eab4296fc45b2a1a5411957617ef
David Chu <davidchuyaya@gmail.com>
2018-02-20 17:22:31 -0500

    creditors can deposit & withdraw

--- contracts/jeffGOLDblum.sol
+++ contracts/jeffGOLDblum.sol
@@ -1,7 +1,22 @@
 pragma solidity ^0.4.0;
 
 contract jeffGOLDblum {
-    function jeffGOLDblum(){
 
+    address[] public creditors;
+    mapping(address => uint) private creditorBalances;
+    mapping(address => uint) public loaners;
+
+    function jeffGOLDblum(address[] _creditors) {
+        creditors = _creditors;
     }
+
+    function deposit() public payable {
+        creditorBalances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        msg.sender.send(creditBalances[msg.sender]);
+    }
+
+
 }
